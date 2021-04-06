60e4352d57151dcc89d4f891ace84338df629e3a
yaobeibei <yaobb@dingdaoso.com>
2021-04-06 11:21:35 +0800

    1

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox
 var cacheStorageKey = 'minimal-pwa-1'
 var cacheList=[
     '/',
+    'favicon.ico',
     'index.html',
 ]
 self.addEventListener('install',e =>{
