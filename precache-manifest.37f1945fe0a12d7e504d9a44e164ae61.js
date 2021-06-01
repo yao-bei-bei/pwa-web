aff48873f2f1db4d486c7a8664b26f30ba253027
yaobeibei <yaobb@dingdaoso.com>
2021-06-01 11:51:40 +0800

    1

---
+++ precache-manifest.37f1945fe0a12d7e504d9a44e164ae61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f40beb92ac954e017d428dee829c1bd",
+    "url": "timg.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "1ad39fb7b5b71f826fb1",
+    "url": "js/chunk-vendors.8fdce233.js"
+  },
+  {
+    "revision": "82e1451cd4096886808a",
+    "url": "js/app.785fe7ef.js"
+  },
+  {
+    "revision": "0f7f03ba6d8ad5a609678298f03438d2",
+    "url": "index.html"
+  },
+  {
+    "revision": "3f40beb92ac954e017d428dee829c1bd",
+    "url": "img/timg.3f40beb9.jpg"
+  },
+  {
+    "revision": "82e1451cd4096886808a",
+    "url": "css/app.944ad219.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.dd7f27293c4f9e71a432bcaf1e720661.js"
+  "precache-manifest.37f1945fe0a12d7e504d9a44e164ae61.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "pwa-demo"});
