b8c5d979f8ea0cae09c2eed3a520db475f16785d
yaobeibei <yaobb@dingdaoso.com>
2021-06-02 10:37:31 +0800

    1

--- service-worker.js
+++ service-worker.js
@@ -14,10 +14,13 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.64d695259c3c545a50d519712d791024.js"
+  "/nippon-color/precache-manifest.7dcbb260b3ef0513033f24007b299bc9.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "pwa-demo"});
+workbox.core.setCacheNameDetails({prefix: "nipponcolor"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
