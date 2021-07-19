88bb8fcd6ecfabb92955db36a336414b13bd1745
要贝贝 <yaobb@dingdao.com>
2021-07-19 14:48:58 +0800

    2

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/pwa-web/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/pwa-web/workbox-v3.6.3"});
 
 importScripts(
-  "/pwa-web/precache-manifest.cea9d45cb393ac7a5cc0a5bf49fb687b.js"
+  "/pwa-web/precache-manifest.2471d1f9055ae8bdabb1bfa32065d2eb.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "nippon-color"});
@@ -38,4 +38,4 @@ workbox.routing.registerNavigationRoute("undefined/index.html", {
 });
 
 workbox.routing.registerRoute(/.*\.js/, workbox.strategies.networkFirst({ "cacheName":"my-js-cache","matchOptions":{"ignoreSearch":true}, plugins: [{ cacheWillUpdate: () => { console.log(`%c js cacheWillUpdate`, `color:#006DCB;`); }, cacheDidUpdate: () => { console.log(`%c js cache update`, `color:#006DCB;`); }, cachedResponseWillBeUsed: () => { console.log(`%c js cachedResponseWillBeUsed`, `color:#006DCB;`); }, requestWillFetch: () => { console.log(`%c js cache requestWillFetch`, `color:#006DCB;`); }, fetchDidFail: () => { console.log(`%c js cache fetchDidFail`, `color:red;`); } }, new workbox.expiration.Plugin({"maxEntries":5,"maxAgeSeconds":60,"purgeOnQuotaError":false}), new workbox.backgroundSync.Plugin("my-jsqueue-name", {"maxRetentionTime":3600}), new workbox.broadcastUpdate.Plugin("my-jsupdate-channel"), new workbox.cacheableResponse.Plugin({"statuses":[200]})] }), 'GET');
-workbox.routing.registerRoute(/http:\/\/open-dev.dingdao.com/, workbox.strategies.networkFirst({ "cacheName":"my-api-cache","matchOptions":{"ignoreSearch":true}, plugins: [{ cacheDidUpdate: () => { console.log(`%c api cache update`, `color:#006DCB;`); } }, new workbox.expiration.Plugin({"maxEntries":5,"maxAgeSeconds":60,"purgeOnQuotaError":false}), new workbox.backgroundSync.Plugin("my-queue-name", {"maxRetentionTime":3600}), new workbox.cacheableResponse.Plugin({"statuses":[200]})] }), 'GET');
+workbox.routing.registerRoute(/.*/, workbox.strategies.networkFirst({ "cacheName":"my-api-cache","matchOptions":{"ignoreSearch":true}, plugins: [{ cacheDidUpdate: () => { console.log(`%c api cache update`, `color:#006DCB;`); } }, new workbox.expiration.Plugin({"maxEntries":5,"maxAgeSeconds":60,"purgeOnQuotaError":false}), new workbox.backgroundSync.Plugin("my-queue-name", {"maxRetentionTime":3600}), new workbox.cacheableResponse.Plugin({"statuses":[200]})] }), 'GET');
