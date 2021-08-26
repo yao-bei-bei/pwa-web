63500b67a6ce73fad42ae3c1d1eacb9f94a58677
要贝贝 <yaobb@dingdao.com>
2021-08-26 12:35:10 +0800

    1

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 // workbox 2.x 是将 workbox 核心内容放在 workbox-sw node_modules 包里维护的
 // workbox 3.x 开始是将 workbox 核心 lib 放在 CDN 维护
 // 当然也可以挪到自己的 CDN 维护
-importScripts('../../workbox.js');
-console.log(workbox)
+importScripts('.workbox.js');
 if (workbox) {
-    console.log(`Yay! workbox is loaded 🎉`);
+  console.log(workbox)
+  console.log(`Yay! workbox is loaded 🎉`);
     // 设置相应缓存的名字的前缀和后缀
     workbox.setConfig({
         debug: false,
